542df57f0fe1d31b237708c929cbbefe68984f7c
ZHANGKH <KhaiZhang@163.com>
2019-08-19 21:20:34 +0800

    [GildedRose:refator take out of update Quality when item is Aged Brie]

--- src/main/java/gildedRose/GildedRose.java
+++ src/main/java/gildedRose/GildedRose.java
@@ -8,36 +8,38 @@ public class GildedRose {
 
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")){
-                    updateNormalItemQuality(items[i]);
-                    continue;
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        updateBackStageQuality(items[i]);
+            if (!items[i].name.equals("Aged Brie")){
+                if(!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")){
+                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")){
+                        updateNormalItemQuality(items[i]);
                         continue;
                     }
-                }
-            }
-
-            reduceSellInIfNotSulfuras(items[i]);
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")){
+                }else {
+                    if (items[i].quality < 50) {
+                        items[i].quality = items[i].quality + 1;
+                        if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                            updateBackStageQuality(items[i]);
                             continue;
                         }
                     }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
                 }
+            } else {
+                updateAgedBrieQuality(items[i]);
+                continue;
+            }
+            reduceSellInIfNotSulfuras(items[i]);
+
+        }
+    }
+
+    private void updateAgedBrieQuality(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
+        }
+        reduceSellInIfNotSulfuras(item);
+        if(item.sellIn < 0){
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
             }
         }
     }
