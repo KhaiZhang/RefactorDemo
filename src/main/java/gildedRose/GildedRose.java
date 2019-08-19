86d8724c7e180434ec71ea8c4a67c2d0f2b5cc03
ZHANGKH <KhaiZhang@163.com>
2019-08-19 21:00:09 +0800

    [GildedRose:refactor take out of update quality when item is normal ]

--- src/main/java/gildedRose/GildedRose.java
+++ src/main/java/gildedRose/GildedRose.java
@@ -10,10 +10,9 @@ public class GildedRose {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie")
                     && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
+                if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")){
+                    updateNormalItemQuality(items[i]);
+                    continue;
                 }
             } else {
                 if (items[i].quality < 50) {
@@ -30,17 +29,25 @@ public class GildedRose {
             if (items[i].sellIn < 0) {
                 if (!items[i].name.equals("Aged Brie")) {
                     if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
+                        continue;
                     }
                 } else {
                     if (items[i].quality < 50) {
                         items[i].quality = items[i].quality + 1;
                     }
                 }
+            }
+        }
+    }
+
+    private void updateNormalItemQuality(Item item) {
+        if (item.quality > 0) {
+                item.quality = item.quality - 1;
+        }
+        reduceSellInIfNotSulfuras(item);
+        if (item.sellIn < 0){
+            if (item.quality > 0) {
+                    item.quality = item.quality - 1;
             }
         }
     }
